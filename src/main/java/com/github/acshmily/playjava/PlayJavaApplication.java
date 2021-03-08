be86f6e6e78d752e8a37fbecbb84cbc9982d530e
acshmily <shmilytq@gmail.com>
2021-03-08 23:55:27 +0800

    tomcat

--- src/main/java/com/github/acshmily/playjava/PlayJavaApplication.java
+++ src/main/java/com/github/acshmily/playjava/PlayJavaApplication.java
@@ -4,12 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-public class PlayJavaApplication {
+public class PlayJavaApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PlayJavaApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PlayJavaApplication.class);
+    }
 }
